7668e1aad36269cc7ffd90dfad4956f3c3954b5f
Arijit-D <Arijitdhar401@gmail.com>
2022-07-21 00:28:22 +0530

    added

---
+++ project part/scripts/project.js
@@ -0,0 +1,12 @@
+
+
+
+ 
+  let popup=document.getElementById("popup");
+  function openpopup(){
+      popup.classList.add("open-popup")
+      body.style.visibility="low"
+  }
+  function closepopup(){
+      popup.classList.remove("open-popup")
+  }
